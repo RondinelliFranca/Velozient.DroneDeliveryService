16b1e4955c4651ce097d7fa8f0550780832504ba
Rondinelli França <rondinelli.franca@gmail.com>
2023-10-13 13:07:14 -0300

    first version

---
+++ Velozient.DroneDeliveryService/Models/Trip.cs
@@ -0,0 +1,31 @@
+﻿namespace Velozient.DroneDeliveryService.Models;
+
+public class Trip
+{
+    public Trip(int numberTrip, List<Location> locations)
+    {
+        NumberTrip = numberTrip;
+        Locations = locations;
+    }
+    public int NumberTrip { get; set; }
+    public List<Location> Locations { get; set; }
+
+    public static void GetTrips(List<Drone> drones, StreamWriter sw)
+    {
+        foreach (var drone in drones.OrderBy(d => d.Name))
+        {
+            sw.WriteLine($"[{drone.Name}]");
+
+            foreach (var droneTrip in drone.Trips.OrderBy(t => t.NumberTrip))
+            {
+                sw.WriteLine($"Trip #{droneTrip.NumberTrip}");
+                foreach (var location in droneTrip.Locations)
+                {
+                    sw.Write(location.Equals(droneTrip.Locations.Last()) ? $"[{location.Name}]" : $"[{location.Name}], ");
+                }
+                sw.WriteLine();
+            }
+            sw.WriteLine(Environment.NewLine);
+        }
+    }
+}
